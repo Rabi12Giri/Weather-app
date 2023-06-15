2364adce2c05ec14d6740fa481d67b4492b09047
Rabi <np01cp4a210220@islingtoncollege.edu.np>
2023-06-15 09:37:02 +0545

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+//c64675bd5535e8e472b0a8547e7586d6
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState({});
+  const [cityInput, setCityInput] = useState("");
+
+  const API_ID = "c64675bd5535e8e472b0a8547e7586d6";
+  const API_URL =
+    "https://api.openweathermap.org/data/2.5/weather?q=kathmandu&appid=" +
+    API_ID;
+
+  const getWeatherDetails = async (cityName) => {
+    const API_ID = "c64675bd5535e8e472b0a8547e7586d6";
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather?";
+
+    const response = await fetch(`${API_URL}q=${cityName}&appid=${API_ID}`);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    setWeatherData(data);
+  };
+
+  const handleSearch = () => {
+    getWeatherDetails(cityInput);
+  };
+
+  const handleChangeInput = (e) => {
+    // console.log("value is changing", e.target.value);
+    setCityInput(e.target.value);
+  };
+
+  useEffect(() => {
+    getWeatherDetails("delhi");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="weather-header">
+        <h1 className="header">Weather Application </h1>
+      </div>
+      <div className="weather-details">
+        <div className="Search Weather">
+          <h2>Search weather</h2>
+        </div>
+        <input
+          type="search"
+          placeholder="search weather"
+          onChange={handleChangeInput}
+          value={cityInput}
+        />
+        <button
+          type="button"
+          className="search-weather-button"
+          onClick={handleSearch}
         >
-          Learn React
-        </a>
-      </header>
+          Search
+        </button>
+        <div className="weather-details-2">
+          <div className="weather-city">
+            <h2>Location name: {weatherData?.name}</h2>
+          </div>
+
+          <div className="weather-temp">
+            <h2>
+              Temperature in celcius:{" "}
+              {(weatherData?.main?.temp - 273.15).toFixed(2)} celcius
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
